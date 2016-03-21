74d1a1e6cd37ccd83a859ebbbcc64a76ac20c862
Conan <conan@nimbledroid.com>
2016-03-21 15:16:19 -0400

    change default route

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'tests#index'
+  root 'apks#index'
 
   get 't' => 'tests#take_test'
 
